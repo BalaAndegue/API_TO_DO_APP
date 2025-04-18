344ce55466033c17271c84b105eb5eef161b8678
Delmat237 <azangueleonel9@gmail.com>
2025-04-18 19:48:34 +0100

    swagger UI fonctionnel , api tasks, categories, user

--- Core/urls.py
+++ Core/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+from Core.ViewSet.CategoryViewSet import CategoryViewSet
+from Core.ViewSet.TaskViewSet import TaskViewSet
+from Core.ViewSet.UserViewSet import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('', views.Home, name='home'),
     path('register/', views.RegisterView, name='register'),
     path('login/', views.LoginView, name='login'),
