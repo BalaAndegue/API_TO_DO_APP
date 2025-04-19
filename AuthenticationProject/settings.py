6790f1306b4ca889ddabf8d6fe9bd23fa3472ed7
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2025-04-19 20:34:50 +0100

    setting pour deployer

--- AuthenticationProject/settings.py
+++ AuthenticationProject/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+ALLOWED_HOSTS = ["BalaAndegue10.pythonanywhere.com"] 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
