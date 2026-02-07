7352fe78199d3ce4630a3624ee343c6fe1c18902
Bala Andegue <balaandeguefrancoislionnel@gmail.com>
2026-02-07 18:13:46 +0100

    BoardViewset made

--- Core/ViewSet/BoardViewSet.py
+++ Core/ViewSet/BoardViewSet.py
@@ -6,3 +6,7 @@ class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Ensure the board.creator is set from the authenticated user
+        serializer.save(creator=self.request.user)
